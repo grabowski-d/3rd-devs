441aa9f12792aa030e5f7f64176cddaf27bfc176
Dariusz Grabowski <39458115+grabowski-d@users.noreply.github.com>
2026-01-07 13:40:52 +0100

    Context: Create init

--- context/py/__init__.py
+++ context/py/__init__.py
@@ -1,1 +1,6 @@
-"""Context module."""
+"""Document context management service."""
+
+from .context_service import ContextService
+from .openai_service import OpenAIService
+
+__all__ = ['ContextService', 'OpenAIService']
