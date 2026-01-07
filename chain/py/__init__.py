24766621b0e87dda2315b558dc96d3930ada3721
Dariusz Grabowski <39458115+grabowski-d@users.noreply.github.com>
2026-01-07 13:30:55 +0100

    Chain: Create init

--- chain/py/__init__.py
+++ chain/py/__init__.py
@@ -1,1 +1,6 @@
-"""Chain module."""
+"""Chain-of-thought QA service."""
+
+from .chain_of_thought import ChainOfThought
+from .openai_service import OpenAIService
+
+__all__ = ['ChainOfThought', 'OpenAIService']
